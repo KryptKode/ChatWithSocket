50810e3f184e39ca613981f0fe353e3c5924c52b
KryptKode <paulakhamiogu@gmail.com>
2019-01-19 16:52:31 +0100

    Connected app to socket.io

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -22,7 +25,10 @@ app.post("/messages", (req, res) =>{
     res.sendStatus(200);
 });
 
+io.on("connection", (socket)=>{
+    console.log("A user connected");
+})
 
-var server = app.listen(3000, ()=>{
+var server = http.listen(3000, ()=>{
     console.log("Server is listening on PORT-", server.address().port)
 });
