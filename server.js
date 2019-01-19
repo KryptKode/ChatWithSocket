27cd688e7af8c0f7b7d2066292aa4759961d61ed
KryptKode <paulakhamiogu@gmail.com>
2019-01-19 18:07:52 +0100

    Fetch and post data to database

--- server.js
+++ server.js
@@ -11,32 +11,44 @@ var dbUrl = "mongodb://username:username1@ds161104.mlab.com:61104/chat"
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false})); 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var messages = [
-    {name:"Tim", message:"Hey"},
-    {name:"Jane", message:"Hullo"}
-];
-
-app.get("/messages", (req, res) =>{
-    console.log("Responding...");
-    res.send(messages);
+var Message = mongoose.model("Message", {
+    name: String,
+    message: String
 });
 
-app.post("/messages", (req, res) =>{
-    messages.push(req.body);
-    io.emit("message", req.body);
-    res.sendStatus(200);
+
+
+app.get("/messages", (req, res) => {
+
+    Message.find({}, (err, data) => {
+        if (err)
+            console.log("Error occurred", err)
+        res.send(data);
+    })
+
 });
 
-io.on("connection", (socket)=>{
+app.post("/messages", (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) => {
+        if (err)
+            sendStatus(500);
+        io.emit("message", req.body);
+        res.sendStatus(200);
+    })
+
+});
+
+io.on("connection", (socket) => {
     console.log("A user connected");
 })
 
-mongoose.connect(dbUrl,{ useNewUrlParser: true }, (err) =>{
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => {
     console.log("Mongoose connected", err);
 })
 
-var server = http.listen(3000, ()=>{
+var server = http.listen(3000, () => {
     console.log("Server is listening on PORT-", server.address().port)
 });
