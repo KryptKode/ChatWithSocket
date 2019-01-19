0be77f4fdba2f9c9e42ffe2db26418213d302b3c
KryptKode <paulakhamiogu@gmail.com>
2019-01-19 16:41:41 +0100

    Post messages from client to server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false})); 
 
 var messages = [
     {name:"Tim", message:"Hey"},
@@ -9,10 +13,16 @@ var messages = [
 ];
 
 app.get("/messages", (req, res) =>{
+    console.log("Responding...");
     res.send(messages);
+});
+
+app.post("/messages", (req, res) =>{
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 
 var server = app.listen(3000, ()=>{
     console.log("Server is listening on PORT-", server.address().port)
-})
+});
